c40de26d31b42e7190d09f6a769f55b7bdbd7337
david0896 <davidvasquez0896@gmail.com>
2023-07-08 14:35:08 -0400

    update height form suscription

--- ym2group_site/components/formularioDeSubscripcion.js
+++ ym2group_site/components/formularioDeSubscripcion.js
@@ -3,7 +3,7 @@ export default function FormularioDeSubscripcion() {
     
     return (
         <div>
-            <iframe src="https://ym2papeleria.ipzmarketing.com/f/azyPcUkJ3OY" frameBorder="0" scrolling="0" className="rounded-3xl h-[11rem] lg:w-[20rem] lg:h-[12rem]"></iframe>
+            <iframe src="https://ym2papeleria.ipzmarketing.com/f/azyPcUkJ3OY" frameBorder="0" scrolling="0" className="rounded-3xl h-[13rem] lg:w-[20rem] lg:h-[12rem]"></iframe>
             <script data-cfasync="false" type="text/javascript" src="https://assets.ipzmarketing.com/assets/signup_form/iframe_v1.js"></script>
         </div>
     )
