d8f26569ec7f58b677a1caef8b6569e7aa6fd090
david0896 <davidvasquez0896@gmail.com>
2023-07-19 12:10:34 -0400

    pagina de mantenimiento

--- ym2group_site/pages/_app.js
+++ ym2group_site/pages/_app.js
@@ -1,9 +1,13 @@
 import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 import '@/styles/globals.css'
 import { ParallaxProvider } from 'react-scroll-parallax';
+import Mantenimiento from './mantenimiento';
 
 export default function App({ Component, pageProps }) {
 
+  const router = useRouter();
+  const enMantenimiento = true;
   useEffect(() => {
     const use = async () => {
       (await import('tw-elements')).default;
@@ -13,7 +17,7 @@ export default function App({ Component, pageProps }) {
 
   return ( 
     <ParallaxProvider>
-      <Component {...pageProps} />
+      {enMantenimiento && router.pathname !== '/mantenimiento' ? <Mantenimiento/> : <Component {...pageProps} />}
     </ParallaxProvider>
   )
 }
