6371d5be26f3393a7859bbb5a2a5e1351a27db29
david0896 <davidvasquez0896@gmail.com>
2023-07-25 19:57:47 -0400

    add google analytics scripts

--- ym2group_site/components/layout.js
+++ ym2group_site/components/layout.js
@@ -4,14 +4,22 @@ import Footer from "./footer"
 
 export default function Layout({children, title = '', description = ''}) {
   return (
-    <div className="">
+    <div>
         <Head>
             <title>{`YM2 GROUP ${title}` }</title>
-            <meta name="description" content={description}/>
+            <meta name="description" content={description}/> 
+            <script async src="https://www.googletagmanager.com/gtag/js?id=G-DTB6V5Z42N"></script>
+            <script>
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments)} 
+              gtag('js', new Date());
+
+              gtag('config', 'G-DTB6V5Z42N');
+            </script>
         </Head>
         <Header/>
         {children}
-        <Footer/>
+        <Footer/>        
     </div>
   )
 }
