63d97454e638524251482076fd81a9d95ee251b1
david0896 <davidvasquez0896@gmail.com>
2023-07-26 03:40:54 -0400

    en mantenimiento - main

--- ym2group_site/pages/_app.js
+++ ym2group_site/pages/_app.js
@@ -7,7 +7,7 @@ import Mantenimiento from './mantenimiento';
 export default function App({ Component, pageProps }) {
 
   const router = useRouter();
-  const enMantenimiento = false;
+  const enMantenimiento = true;
   useEffect(() => {
     const use = async () => {
       (await import('tw-elements')).default;
